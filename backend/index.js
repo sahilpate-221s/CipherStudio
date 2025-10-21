5f8cfa097f4b62ab8ac607c576a9f8a184f84d8a
Sahil Patel <112198439+sahilpate-221s@users.noreply.github.com>
2025-10-22 04:58:15 +0530

    few changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "http://localhost:5173", // frontend URL
+  origin: process.env.FRONTEND_URL || "http://localhost:5173", // frontend URL
   credentials: true,
 }));
 
